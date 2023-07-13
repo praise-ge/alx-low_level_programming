e40468b40ffca0145126068c91582bddbac54bac
praise-ge <praiseoluyemi8@gmail.com>
2023-07-13 14:13:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
+#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/syscall.h>
 
-int main() {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    syscall(SYS_write, STDERR_FILENO, message, strlen(message));
-    return 1;
+/**
+ * main - Entry point
+ *
+ * REturn: Always 1 (Success)
+ */
+int main(void)
+{
+        write(2,
+        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
 }
-
