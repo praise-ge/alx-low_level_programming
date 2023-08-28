76d0954b50b01a7f6bf520f4e9388d4546739877
praise-ge <praiseoluyemi8@gmail.com>
2023-08-28 10:16:19 -0700

    second

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,12 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/*
+/**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
  * for Holberton project
  */
