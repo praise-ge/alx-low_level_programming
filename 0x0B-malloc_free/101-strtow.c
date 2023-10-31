c2ebc6ff78b59ecd0c73c7a260b47c2b41f83f19
praise-ge <praiseoluyemi8@gmail.com>
2023-10-31 07:58:39 -0700

    ini

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -57,20 +57,19 @@ char **strtow(char *str)
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
-																																										while (start < end)
-																																																*tmp++ = str[start++];
-																																														*tmp = '\0';
-																																																		matrix[k] = tmp - c;
-																																																						k++;
-																																																										c = 0;
-																																																													}
-																							}
-																else if (c++ == 0)
-																				start = i;
-																	}
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
 
-										matrix[k] = NULL;
-
-											return (matrix);
+	matrix[k] = NULL;
+	return (matrix);
 }
 
